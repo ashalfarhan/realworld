a6a706596142a7ae74f1092de6d3f5cb69e9b288
ashalfarhan <ashalfarhan8@gmail.com>
2022-01-29 17:44:41 +0700

    config init

---
+++ config/config.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+type Config struct {
+	Addr string
+	Port string
+	// Should be "dev" or "prod"
+	Env string
+}
+
+func LoadConfig() *Config {
+	var ok bool
+	co := &Config{}
+
+	co.Port, ok = os.LookupEnv("PORT")
+	if !ok {
+		co.Port = "4000"
+	}
+	log.Println("Using port:", co.Port)
+
+	host := os.Getenv("HOST")
+
+	co.Addr = fmt.Sprintf("%s:%s", host, co.Port)
+
+	co.Env, ok = os.LookupEnv("APP_ENV")
+	if !ok {
+		co.Env = "dev"
+		log.Println("APP_ENV is not set, using default \"dev\"")
+	}
+
+	return co
+}
