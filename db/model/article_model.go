39a54ed26223a0336e9679d744c855aaf3fcea2b
ashalfarhan <ashalfarhan8@gmail.com>
2022-01-30 15:01:35 +0700

    modify some model

--- db/model/article_model.go
+++ db/model/article_model.go
@@ -1,14 +1,13 @@
 package model
 
 type Article struct {
-	Slug        string   `json:"slug"`
-	Title       string   `json:"title"`
-	Description string   `json:"description"`
-	Body        string   `json:"body"`
-	TagList     []string `json:"tagList"`
-	CreatedAt   string   `json:"createdAt"`
-	UpdatedAt   string   `json:"updatedAt"`
+	Slug        string `json:"slug"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Body        string `json:"body"`
+	CreatedAt   string `json:"createdAt"`
+	UpdatedAt   string `json:"updatedAt"`
 	// Favorited      bool     `json:"favorited"`
-	FavoritesCount int  `json:"favoritesCount"`
-	Author         User `json:"author"`
+	// FavoritesCount int  `json:"favoritesCount"`
+	Author *User `json:"author"`
 }
