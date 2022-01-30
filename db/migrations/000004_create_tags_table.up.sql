7aea3c1cc80a0dc4a08770479db7fdf7d69bce5a
ashalfarhan <ashalfarhan8@gmail.com>
2022-01-30 16:56:31 +0700

    change some migrations

--- db/migrations/000004_create_tags_table.up.sql
+++ db/migrations/000004_create_tags_table.up.sql
@@ -1,3 +1,9 @@
-CREATE TABLE IF NOT EXISTS tags (
-    name VARCHAR(255) PRIMARY KEY
+CREATE TABLE IF NOT EXISTS article_tags (
+    article_id UUID NOT NULL,
+    tag_name VARCHAR(255) NOT NULL,
+    PRIMARY KEY(article_id, tag_name),
+    CONSTRAINT fk_article
+        FOREIGN KEY(article_id)
+        REFERENCES articles(id)
+        ON DELETE CASCADE
 );
