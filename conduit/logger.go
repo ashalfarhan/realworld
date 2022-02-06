a740a31e2f28522c824b942d423b165405a8aac6
ashalfarhan <ashalfarhan8@gmail.com>
2022-02-06 23:58:07 +0700

    fix: logger to show file

--- conduit/logger.go
+++ conduit/logger.go
@@ -6,8 +6,8 @@ import (
 	"os"
 )
 
-var Logger = NewLogger("conduit-app")
+var Logger = log.New(os.Stdout, "[conduit-app] ", log.LstdFlags|log.Lmsgprefix)
 
 func NewLogger(prefix string) *log.Logger {
-	return log.New(os.Stdout, fmt.Sprintf("[%s] ", prefix), log.LstdFlags|log.Lmsgprefix)
+	return log.New(os.Stdout, fmt.Sprintf("[%s] ", prefix), log.LstdFlags|log.Lmsgprefix|log.Lshortfile)
 }
