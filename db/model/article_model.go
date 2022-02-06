ba23182de0801e050c3fc087333ee2dc547aed73
ashalfarhan <ashalfarhan8@gmail.com>
2022-02-06 23:57:38 +0700

    feat: add serialzer

--- db/model/article_model.go
+++ db/model/article_model.go
@@ -1,7 +1,9 @@
 package model
 
+import "encoding/json"
+
 type Article struct {
-	ID             string   `json:"-" db:"id"`
+	ID             string   `json:"id" db:"id"`
 	Slug           string   `json:"slug" db:"slug"`
 	Title          string   `json:"title" db:"title"`
 	Description    string   `json:"description" db:"description"`
@@ -13,4 +15,58 @@ type Article struct {
 	Favorited      bool     `json:"favorited"`
 	FavoritesCount int      `json:"favoritesCount"`
 	Author         *User    `json:"author" db:"author"`
+}
+
+type ArticleSerialized struct {
+	Slug           string   `json:"slug"`
+	Title          string   `json:"title"`
+	Description    string   `json:"description"`
+	Body           string   `json:"body"`
+	CreatedAt      string   `json:"createdAt"`
+	UpdatedAt      string   `json:"updatedAt"`
+	TagList        []string `json:"tagList"`
+	Favorited      bool     `json:"favorited"`
+	FavoritesCount int      `json:"favoritesCount"`
+	Author         *User    `json:"author"`
+}
+
+func (a Article) Serialize() *ArticleSerialized {
+	return &ArticleSerialized{
+		a.Slug,
+		a.Title,
+		a.Description,
+		a.Body,
+		a.CreatedAt,
+		a.UpdatedAt,
+		a.TagList,
+		a.Favorited,
+		a.FavoritesCount,
+		a.Author,
+	}
+}
+
+func (a Article) MarshalBinary() ([]byte, error) {
+	return json.Marshal(a)
+}
+
+func (a *Article) UnmarshalBinary(data []byte) error {
+	return json.Unmarshal(data, a)
+}
+
+type Articles []*Article
+
+func (as Articles) Serialize() []*ArticleSerialized {
+	ars := []*ArticleSerialized{}
+	for _, a := range as {
+		ars = append(ars, a.Serialize())
+	}
+	return ars
+}
+
+func (a Articles) MarshalBinary() ([]byte, error) {
+	return json.Marshal(a)
+}
+
+func (a *Articles) UnmarshalBinary(data []byte) error {
+	return json.Unmarshal(data, a)
 }
--- db/model/comment_model.go
+++ db/model/comment_model.go
@@ -1,5 +1,7 @@
 package model
 
+import "encoding/json"
+
 type Comment struct {
 	ID        string `json:"id" db:"id"`
 	Body      string `json:"body" db:"body"`
@@ -8,4 +10,14 @@ type Comment struct {
 	Author    *User  `json:"author"`
 	CreatedAt string `json:"createdAt" db:"created_at"`
 	UpdatedAt string `json:"updatedAt" db:"updated_at"`
+}
+
+type Comments []*Comment
+
+func (a Comments) MarshalBinary() ([]byte, error) {
+	return json.Marshal(a)
+}
+
+func (a *Comments) UnmarshalBinary(data []byte) error {
+	return json.Unmarshal(data, a)
 }
