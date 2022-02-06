6964b03ec6b354989cb74374679d744041affe68
ashalfarhan <ashalfarhan8@gmail.com>
2022-02-06 14:19:17 +0700

    fix: adjust model

--- db/model/comment_model.go
+++ db/model/comment_model.go
@@ -1,11 +1,11 @@
 package model
 
 type Comment struct {
-	ID        int    `json:"id" db:"id"`
+	ID        string `json:"id" db:"id"`
 	Body      string `json:"body" db:"body"`
 	ArticleID string `json:"-" db:"article_id"`
 	AuthorID  string `json:"-" db:"author_id"`
-	Author    User   `json:"author"`
+	Author    *User   `json:"author"`
 	CreatedAt string `json:"createdAt" db:"created_at"`
 	UpdatedAt string `json:"updatedAt" db:"updated_at"`
 }
