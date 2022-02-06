e4dc72218ef7053b6499ad1709e5e22a6c5abc91
ashalfarhan <ashalfarhan8@gmail.com>
2022-02-06 23:59:10 +0700

    feat: redis cache service

---
+++ service/cache_service.go
@@ -0,0 +1,41 @@
+package service
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/ashalfarhan/realworld/conduit"
+	"github.com/go-redis/redis/v8"
+)
+
+type CacheService struct {
+	store  *redis.Client
+	logger *log.Logger
+}
+
+const (
+	defaultTTL = time.Hour * 2
+)
+
+func NewCacheService(s *redis.Client) *CacheService {
+	return &CacheService{
+		s,
+		conduit.NewLogger("cache-service"),
+	}
+}
+
+func (s *CacheService) Set(ctx context.Context, key string, value interface{}) {
+	if err := s.store.SetEX(ctx, key, value, defaultTTL).Err(); err != nil {
+		s.logger.Printf("Cannot saving cache. reason=%v\n", err)
+	}
+}
+
+func (s *CacheService) Get(ctx context.Context, key string, dest interface{}) bool {
+	err := s.store.Get(ctx, key).Scan(dest)
+	if err != nil {
+		s.logger.Printf("Cannot restore cache. key=%s reason=%v\n", key, err)
+	}
+
+	return err == nil
+}
