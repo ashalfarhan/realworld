52e138798540f2b25a367f1e23d564ca4f0f959c
ashalfarhan <ashalfarhan8@gmail.com>
2022-06-26 02:39:20 +0700

    adjust some

--- test/http.go
+++ test/http.go
@@ -1,31 +1,31 @@
 package test
 
-import (
-	"bytes"
-	"encoding/json"
-	"log"
-	"net/http"
-	"net/http/httptest"
-)
+// import (
+// 	"bytes"
+// 	"encoding/json"
+// 	"log"
+// 	"net/http"
+// 	"net/http/httptest"
+// )
 
-func MakeRequest(method, target string, payload, bodyDest interface{}, handler http.HandlerFunc) *http.Response {
-	b, err := json.Marshal(payload)
-	if err != nil {
-		log.Panicf("Cannot marshal payload: %v, Reason: %v\n", payload, err)
-		return nil
-	}
+// func MakeRequest(method, target string, payload, dest interface{}, handler http.HandlerFunc) *http.Response {
+// 	b, err := json.Marshal(payload)
+// 	if err != nil {
+// 		log.Panicf("Cannot marshal payload: %v, Reason: %v\n", payload, err)
+// 		return nil
+// 	}
 
-	req := httptest.NewRequest(method, target, bytes.NewReader(b))
-	w := httptest.NewRecorder()
+// 	req := httptest.NewRequest(method, target, bytes.NewReader(b))
+// 	w := httptest.NewRecorder()
 
-	handler(w, req)
-	res := w.Result()
+// 	handler(w, req)
+// 	res := w.Result()
 
-	defer res.Body.Close()
-	if err := json.NewDecoder(res.Body).Decode(bodyDest); err != nil {
-		log.Panicf("Cannot Decode response body, Reason: %v\n", err)
-		return nil
-	}
+// 	defer res.Body.Close()
+// 	if err := json.NewDecoder(res.Body).Decode(dest); err != nil {
+// 		log.Panicf("Cannot Decode response body, Reason: %v\n", err)
+// 		return nil
+// 	}
 
-	return res
-}
+// 	return res
+// }
